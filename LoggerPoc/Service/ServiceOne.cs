226797ef1031b13f35a3e7c4241a48c8737ccfbf
Floyd Pink <imbleedingme@googlemail.com>
2012-12-15 19:54:58 -0500

    added an example of an organic logging as well
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- LoggerPoc/Service/ServiceOne.cs
+++ LoggerPoc/Service/ServiceOne.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using log4net;
 
 namespace LoggerPoc.Service
 {
     public class ServiceOne : IServiceOne
     {
+        private ILog _log;
+
+        public ServiceOne(ILog log)
+        {
+            _log = log;
+        }
+
+        public ILog Log
+        {
+            get { return _log; }
+        }
+
         public void Foo()
         {
+            Log.Info("Example of organic logging from the business object. Staring application.");
             Console.WriteLine("In ServiceOne.Foo");
         }
 
